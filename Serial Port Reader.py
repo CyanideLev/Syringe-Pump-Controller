f9e42406857f58bb7d05a4f317aa6731ebb8c66e
CyanideLev <142946073+CyanideLev@users.noreply.github.com>
2025-09-24 16:30:49 -0600

    Test code for port reader

--- Serial Port Reader.py
+++ Serial Port Reader.py
@@ -1,8 +1,24 @@
 import serial
 import time 
-import os, sys
+import keyboard
 
-port = serial.Serial('COM3', 19200, parity=serial.PARITY_NONE, bytesize=8, stopbits=1, timeout=None, xonxoff=0, rtscts=0) #Open serial port using settings required by pump on COM3
+def key_press(event):   #Break loop when Q is pressed on keyboard
+    global running
+    if event.name == 'q':
+        running = False
 
-def read_cmd(cmd):
-    port.read()
+def serial_listen():
+    running = True
+    keyboard.on_press(key_press)
+
+    while running:
+        port = serial.Serial('COM3', 19200, parity=serial.PARITY_NONE, bytesize=8, stopbits=1, timeout=None, xonxoff=0, rtscts=0) #Open serial port using settings required by pump on COM3
+        x = serial.readline()
+        
+        print (x)
+    
+    port.close()
+    print("Program ended by keypress.")
+
+if __name__ == "__main__":
+    serial_listen()
