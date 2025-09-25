ad5a139de5c595672d52927c566e9ee45078f114
CyanideLev <berdanbrandon@gmail.com>
2025-09-25 15:27:03 -0600

    Removed keyboard module, added cv2

--- Serial Port Reader.py
+++ Serial Port Reader.py
@@ -1,22 +1,21 @@
 import serial
 import time 
-import keyboard
-
-def key_press(event):   #Break loop when Q is pressed on keyboard
-    global running
-    if event.name == 'q':
-        running = False
+import cv2
 
 def serial_listen():
-    port = serial.Serial('COM4', 19200, parity=serial.PARITY_NONE, bytesize=8, stopbits=1, timeout=None, xonxoff=0, rtscts=0)
     
-    running = True
-    keyboard.on_press(key_press)
+    port = serial.Serial('COM4', 19200, parity=serial.PARITY_NONE, bytesize=8, stopbits=1, timeout=None, xonxoff=0, rtscts=0)
 
-    while running:
+    print ('Listening...')
+
+    while (True):
+        
         x = port.readline()
         
         print (x + '\r\n')
+        
+        if cv2.waitkey(1)==ord('q'):
+            break
     
     port.close()
     print("Program ended by keypress.")
